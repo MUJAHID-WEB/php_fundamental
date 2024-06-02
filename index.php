3636c0a5d902f8589233a3627dee5d11e37debf3
MUJAHID-WEB <mujahid30390@gmail.com>
2024-06-02 17:43:46 +0600

    loop

--- index.php
+++ index.php
@@ -26,6 +26,7 @@
 // if else
     $a = 5;
     $b = 3;
+    $c = 2;
     if ($a < $b){
         echo "A is bigger";
     }else if($a > $b){
@@ -36,7 +37,96 @@
 
     echo " </br></br>" ;
 
-    //
+    // Operators: + - * / % **, = += -= /= *= %= , == ===  !== > < >= <=, , &&, ||, !
+    
+    echo $a%$b;
+
+    echo " </br></br>" ;
+
+    echo $a += 10;
+
+    echo " </br></br>" ;
+
+    if ($a === $b){
+        echo "A and B is equal";
+    }else{
+        echo "false" ;
+    }
+
+    echo " </br></br>" ;
+
+    $a++;
+    echo $a;
+
+    echo " </br></br>" ;
+
+    if($a > $b && $a > $c){
+        echo "A";
+    }
+
+    echo " </br></br>" ;
+
+    if($a > $b || $a > $c){
+        echo "A";
+    }
+
+    echo " </br></br>" ;
+
+    // switch
+    switch($a){
+        case($a > $b && $a < $c):
+            echo "A";
+
+            break;
+        default:
+            echo "False";
+    }
+
+
+
+    echo " </br></br>" ;
+    // Loop: For, While, Dowhile, forEach
+
+
+    for ($i = 1; $i <= 100; $i++) {
+        echo $i.', ';
+    }
+
+    echo " </br></br>" ;
+
+    for ($i = 1; $i <= 100; $i++) {
+        if($i%2 != 0){
+            echo $i.', ';
+        }
+    }
+
+
+    echo " </br></br>" ;
+
+    $j = 1;
+
+    while($j <= 50 ){
+        echo $j.', ';
+        $j++;
+    }
+
+    echo " </br></br>" ;
+
+
+    echo " </br></br>" ;
+
+
+    echo " </br></br>" ;
+
+
+    echo " </br></br>" ;
+
+
+    echo " </br></br>" ;
+
+
+    echo " </br></br>" ;
+
 
     ?>
 </body>
